710e99ebe7b844ec1f01fc29cc933074493f36dd
Obai Basher <53649412+ObaiBasheer@users.noreply.github.com>
2023-10-23 15:56:47 +0300

    add Models

---
+++ Tricking.Library.API/Controllers/Tricks.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tricking.Library.API.Controllers
+{
+    [ApiController]
+    [Route("api/Tricks")]
+    public class Tricks : ControllerBase
+    {
+
+    }
+}
---
+++ Tricking.Library.API/Models/Trick.cs
@@ -0,0 +1,8 @@
+﻿namespace Tricking.Library.API.Models
+{
+    public class Trick
+    {
+        public int Id { get; set; } 
+        public string? Name { get; set; }
+    }
+}
